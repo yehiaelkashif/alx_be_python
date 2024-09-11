133fd6badb7a80ee5fec727788e9c0bf2fec3d54
yehiasalah <yehiasalah74@gmail.com>
2024-09-12 00:46:56 +0300

    operations first commit

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,15 @@
+number1=10
+number2=5
+sumation=number1+number2
+difference=number1- number2
+product=number1*number2
+
+print(f"Addition of {number1} and {number2}  is {sumation}" )  ## print sum of two Numbers 
+print(f"Subtraction  of {number1} and {number2}  is {difference}" )  ## print difference  of two Numbers
+print(f"Multiplication  of {number1} and {number2}  is {product}" )  ## print product  of two Numbers 
+
+
+
+
+
+
